84df56f67b9f78b55c487d7e1dbd8d7ff3025637
Mao Lijie <unixlikeos@gmail.com>
2010-11-02 16:13:43 +0800

    create temporary folder at the beginnig, delete them at last

--- find.sh
+++ find.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-set -x
+#set -x
 
 echo "find -name with -path and -prune"
 
@@ -8,6 +8,9 @@ echo "find -name with -path and -prune"
 if test -d t1 || test -d t2 || test -d t3
 then
 	rm -fr t{1,2,3}
+	mkdir t{1,2,3}
+	touch t{1,2,3}/1.tmp
+else 
 	mkdir t{1,2,3}
 	touch t{1,2,3}/1.tmp
 fi
@@ -24,4 +27,8 @@ find . \( -path ./t1 -o -path ./t2 \) -prune -o -name "1.tmp" -print
 # 2: absolute path , result show as absolute path
 echo "2: absolute path , result show as absolute path"
 find `pwd` \( -path `pwd`/t1 -o -path `pwd`/t2 \) -prune -o -name "1.tmp" -print
+
+#delete the 3 temporary directory
+rm -fr t{1,2,3}
+
 exit $?
