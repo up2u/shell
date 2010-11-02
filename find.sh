17ffa022e1eaba6c1f37a406fc396be416846535
Mao Lijie <unixlikeos@gmail.com>
2010-11-02 10:52:29 +0800

    shell command : find mainly about find -name with -path and -prune

---
+++ find.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+set -x
+
+echo "find -name with -path and -prune"
+
+#if not exist
+#create 3 temporary directory
+#touch a temporary file for every directory
+if ! test -d t{1,2,3}
+then
+	mkdir t{1,2,3}
+	touch t{1,2,3}/1.tmp
+fi
+
+#find
+find . \( -path t1/ -o -path t2/ \) -prune -o -name "1.tmp" -print
+
+exit $?
