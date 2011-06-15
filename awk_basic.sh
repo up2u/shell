0e0dc4cdc05f1ec30b5e143929b86ce8b61c9379
Mao Lijie <unixlikeos@gmail.com>
2011-06-15 17:46:59 +0800

    awk conditions (without ;)

--- awk_basic.sh
+++ awk_basic.sh
@@ -2,4 +2,7 @@
 
 echo -e "1 \n 2 \n a \n b \n jlj\n" | awk 'BEGIN {print "this head"} {print $1}  END{print "this is tail"}' 
 
+# print line number fullfil the condions (without ; between condtion !!!)
+echo -e "1 4 \n 6 8 \n 78 56\n9 1" | awk '$1<10{x++} $2>5{y++} END{print x, y}'
+
 exit $?
