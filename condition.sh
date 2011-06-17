0244304f4e7f6e25dc905a94edb9eb6f0cc1e26c
Mao Lijie <unixlikeos@gmail.com>
2011-06-17 15:17:58 +0800

    1: strstr 2: conditions use \ and [] , |

--- condition.sh
+++ condition.sh
@@ -1,6 +1,10 @@
 #! /bin/sh
 # input ./sh  1 2 
 
+## use backslash with conditions !!!
+# [ -f /var/lock/subsys/$subsys -o -f /var/lock/subsys/$subsys.init ] \ 
+#		|| continue
+
 ## for logical AND
 #	if [ "$1" -eq "1" -a "$2" -eq "2" ]; then
 	if [ "$1" -eq "1" ] && [ "$2" -eq "2" ]; then     # with same effect
