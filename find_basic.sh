b12aa6d304d608efe217e6cfac341bb12270d282
Mao Lijie <unixlikeos@gmail.com>
2011-06-23 15:26:11 +0800

    find_basic.sh : find basics commands.txt : commands for studying

---
+++ find_basic.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+set -x
+
+# rename , use exec
+touch 1.txt
+find . -name "1.txt" -exec mv {} {}.log \;
+rm 1.txt.log
+
+# ignore path ./.git
+# perm
+
+find . -path ./.git  -prune -o -perm  111     # match exectuable bit was set,and only exectuable bit was set
+find . -path ./.git  -prune -o -perm /111     # match any exectuable bit was set 
+find . -path ./.git  -prune -o -perm -111     # match all exectuable bit was set 
+
+
+exit $?
