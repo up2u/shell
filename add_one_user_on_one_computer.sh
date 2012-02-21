75513946f795bf3e3a992befd70a8399da2d8f63
Mao Lijie <maolijie@andestech.com.cn>
2012-02-21 11:18:40 +0800

    oprofile.txt 	steps of how to use oprofile add_one_user_on_one_computer.sh 	adjust added user group

--- add_one_user_on_one_computer.sh
+++ add_one_user_on_one_computer.sh
@@ -10,7 +10,7 @@ set -x
 	fi
 
 	grep $1 /etc/passwd && echo this computer have been added this user\
-			 || /usr/sbin/useradd -c $1 -g $2 -G andeshz -d /andesHZ/users/$1 -m $1 -u $3\
+			 || /usr/sbin/useradd -c $1 -g $2 -G andeswh -d /andesWH/SSH-dir/$1 -m $1 -u $3\
 		    && echo $1|passwd --stdin $1 && echo Add user $1 successful in pc
 
 exit 0
